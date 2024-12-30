27837433693e03bc9beb55b3d1428128c93d7b55
AK1239 <akilkhatri235@gmail.com>
2024-12-30 18:14:59 +0300

    modified hamburger icon

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 // Menu toggle functionality
 const menuToggle = document.getElementById("menu-toggle");
 const mobileMenu = document.getElementById("mobile-menu");
-const hamburgerLines = document.querySelectorAll(".hamburger-line");
+const menuIcon = document.getElementById("menu-icon");
 
 menuToggle.addEventListener("click", () => {
   // Toggle mobile menu
   mobileMenu.classList.toggle("hidden");
 
-  // Animate hamburger to X
-  hamburgerLines[0].classList.toggle("rotate-45");
-  hamburgerLines[0].classList.toggle("translate-y-2");
-  hamburgerLines[1].classList.toggle("opacity-0");
-  hamburgerLines[2].classList.toggle("-rotate-45");
-  hamburgerLines[2].classList.toggle("-translate-y-2");
+  // Toggle between hamburger and X icons
+  menuIcon.classList.toggle("fa-bars");
+  menuIcon.classList.toggle("fa-times");
 });
 
 // Close mobile menu when clicking outside
@@ -20,10 +17,9 @@ document.addEventListener("click", (e) => {
   if (!menuToggle.contains(e.target) && !mobileMenu.contains(e.target)) {
     mobileMenu.classList.add("hidden");
 
-    // Reset hamburger animation
-    hamburgerLines[0].classList.remove("rotate-45", "translate-y-2");
-    hamburgerLines[1].classList.remove("opacity-0");
-    hamburgerLines[2].classList.remove("-rotate-45", "-translate-y-2");
+    // Reset to hamburger icon
+    menuIcon.classList.add("fa-bars");
+    menuIcon.classList.remove("fa-times");
   }
 });
 
