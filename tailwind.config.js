a26ec78ea5d5a82327d9fef1523a09591e6aacde
AK1239 <akilkhatri235@gmail.com>
2024-12-30 18:18:03 +0300

    added custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sansum-blue": "#1e3c8a",
+        "sansum-red": "#e40010",
+      },
+    },
   },
   plugins: [],
 };
