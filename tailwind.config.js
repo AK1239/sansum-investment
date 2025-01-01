a72fdabc3b367c97c580e4a9e84469fe62c11f87
AK1239 <akilkhatri235@gmail.com>
2025-01-01 19:30:37 +0300

    added why choose us section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
         "sansum-blue": "#1e3c8a",
         "sansum-red": "#e40010",
       },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 1s ease-out forwards",
+      },
     },
   },
   plugins: [],
